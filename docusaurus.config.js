b15f031ba0855e00a469ae7948ddbb4f3adb922c
hamidbakhtiari <hamid331994@gmail.com>
2020-08-14 18:09:29 +0430

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,105 @@
+module.exports = {
+  title: 'Themy',
+  tagline: 'lightweight Js theme toggler',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'hamid814', // Usually your GitHub org/user name.
+  projectName: 'themy', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'themy',
+      logo: {
+        alt: 'themy',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'right',
+        },
+        { to: 'blog', label: 'Blog', position: 'right' },
+        {
+          href: 'https://github.com/hamid814/themy',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'light',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Contact Me',
+          items: [
+            {
+              label: 'Email',
+              href: 'mailto:hamid331994@gmail.com',
+            },
+            {
+              label: 'Github',
+              href: 'https://github.com/hamid814',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/hamid331994',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} themy by hamid814. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: 'installation',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/hamid814/themy-doc/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
